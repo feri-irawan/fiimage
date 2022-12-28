b3319cb14a5a59452de1fd5904023d220019420a
Feri Irawan <feriirawan.akun@gmail.com>
2022-12-28 11:26:00 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# fiimage
+
+Resize image and create a thumbnail of video, made with ❤ by Feri Irawan at 28/12/2022
---
+++ index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import ffmpegPath from "@ffmpeg-installer/ffmpeg";
+import genThumbnail from "simple-thumbnail";
+
+const server = express();
+
+// Membuat thumbnail
+server.get("/", async (req, res) => {
+  let { url, w, t } = req.query;
+
+  if (!url) {
+    res.status(403).send("Bad request");
+    return;
+  }
+
+  try {
+    await genThumbnail(url, res, w ? `${w}x?` : "100x?", {
+      path: ffmpegPath.path,
+      seek: t,
+    });
+  } catch (error) {}
+});
+
+server.listen(3000);
+
+export default server;
